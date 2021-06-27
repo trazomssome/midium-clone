5db1ffb578eee3253e0208bd66746f2eeb79ff52
trazomssome <rkdtmdwn@gmail.com>
2021-06-27 21:08:21 +0900

    로그인 파트 진행 중

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-const form = document.querySelector(".check-name");
+const form = document.querySelector(".login-form");
+const loginInput = form.querySelector(".input");
+const greeting = document.querySelector(".greeting");
 
 const handleSubmit = (event) =>{
     event.preventDefault();
-    console.log("submit");
+    form.classList.add("hidden");
+    greeting.classList.remove("hidden");
+    console.log(loginInput);
+    const username = loginInput.value;
+    greeting.innerText = `Hello, ${username}`;
 };
 
 form.addEventListener("submit", handleSubmit);
