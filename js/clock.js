328ad670f367958b8f73b45a4f8eeba8734aa337
trazomssome <rkdtmdwn@gmail.com>
2021-06-27 22:31:21 +0900

    login 완료

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,14 @@
 const clock = document.querySelector(".clock");
 
-const checkTime = () =>{
-    const current = new Date();
-    const hours = current.getHours();
-    const minutes = current.getMinutes();
-    const seconds = current.getSeconds();
-    clock.innerText = `${String(hours).padStart(2,"0")}:${String(minutes).padStart(2,"0")}:${String(seconds).padStart(2,"0")}`
-}
+const checkTime = () => {
+  const current = new Date();
+  const hours = current.getHours();
+  const minutes = current.getMinutes();
+  const seconds = current.getSeconds();
+  clock.innerText = `${String(hours).padStart(2, "0")}:${String(
+    minutes
+  ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+};
 
 checkTime();
 setInterval(checkTime, 1000);
--- js/index.js
+++ js/index.js
@@ -2,13 +2,25 @@ const form = document.querySelector(".login-form");
 const loginInput = form.querySelector(".input");
 const greeting = document.querySelector(".greeting");
 
-const handleSubmit = (event) =>{
-    event.preventDefault();
-    form.classList.add("hidden");
-    greeting.classList.remove("hidden");
-    console.log(loginInput);
-    const username = loginInput.value;
-    greeting.innerText = `Hello, ${username}`;
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  form.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 };
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  console.log("nono");
+} else {
+  form.classList.toggle(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello, ${savedUsername}`;
+  greeting.classList.toggle(HIDDEN_CLASSNAME);
+}
 form.addEventListener("submit", handleSubmit);
