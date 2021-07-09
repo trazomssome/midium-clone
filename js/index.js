b417c4b3c334da1994a6c0c22a0694727ceb9cb3
trazomssome <trazomssome@gmail.com>
2021-07-09 14:01:46 +0900

    login 관련 css 수정

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const form = document.querySelector(".login-form");
 const loginInput = form.querySelector(".input");
 const greeting = document.querySelector(".greeting");
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const mainNoLogin = document.querySelector(".main__wrapper__no-login");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -16,11 +20,17 @@ const checkLogin = () => {
   const savedUsername = localStorage.getItem(USERNAME_KEY);
 
   if (savedUsername === null) {
-    form.classList.remove(HIDDEN_CLASSNAME);
     greeting.classList.add(HIDDEN_CLASSNAME);
+    header.classList.add(HIDDEN_CLASSNAME);
+    main.classList.add(HIDDEN_CLASSNAME);
+    footer.classList.add(HIDDEN_CLASSNAME);
+    mainNoLogin.classList.remove(HIDDEN_CLASSNAME);
   } else {
-    form.classList.add(HIDDEN_CLASSNAME);
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    mainNoLogin.classList.add(HIDDEN_CLASSNAME);
+    header.classList.remove(HIDDEN_CLASSNAME);
+    main.classList.remove(HIDDEN_CLASSNAME);
+    footer.classList.remove(HIDDEN_CLASSNAME);
     let now = new Date().getHours();
     if (6 < now && now < 12) {
       greeting.innerText = `Good morning, ${savedUsername}`;
